812f0208d4d4d9a127ef222e3e0afc2ded521211
bourghce <bourghce@mail.uc.edu>
2022-09-19 00:37:04 -0400

    powershell files

---
+++ powershell/Lab3_WinSysInfo.ps1
@@ -0,0 +1,13 @@
+﻿########## Lab 3    ##Christine Bourgholtzer    ##9/18/2022 ##########
+
+#Define variables to be called in BODY message/variable
+$User = [system.environment]::username
+$Hostname = hostname
+$PSversion = $HOST.version.Major
+$Date = Get-Date -DisplayHint Date
+
+#Define BODY variable
+$BODY = 'This machine''s IP is {0}. User is {1}. Hostname is {2}. PowerShell Version {3}. Today''s Date is {4}.' -f $IP, $User, $Hostname, $PSversion, $Date
+
+#Call/Display variable output
+$BODY
