7e6c9a0fec6e950ebf01e95a74b9f27f02009554
bourghce <bourghce@mail.uc.edu>
2022-10-02 21:02:26 -0400

    myAge and myName var logic

--- Python/nameAge.py
+++ Python/nameAge.py
@@ -7,8 +7,31 @@ start_time = time.time()
 
 print('What is your name?')
 myName = input()
+
+# continuously ask for name until it is correct - LOOP 
+while myName != 'Christine':
+    if myName == 'your name':
+        print('Ha ha, very funny. Seriously, who are you?')
+        myName = input()
+    else:        
+        print('This is not your name. Please tell me your actual name.')
+        myName = input()
+
 print('Hello, ' + myName + '. That is a good name. How old are you?')
 myAge = int(input())
+
+# add logic
+if myAge < 13:
+    print("Learning young, that's good")
+elif myAge == 13:
+    print("You're a teenager now... that's cool, I guess")
+elif myAge > 13 and myAge < 30:
+    print("Still young, still learning...")
+elif myAge >= 30 and myAge < 65:
+    print("Now you're adulting.")
+else:
+    print("... you've lived a long time?")
+
 programAge = int(time.time() - start_time)
 print('%s? That\'s funny, I\'m only %s seconds old.' % (myAge, programAge))
 
